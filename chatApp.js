94a60fa1738959c4cbe1271075fc71275e67ec8b
Elias Peeters <Elias.peeters@me.com>
2022-02-24 17:17:16 +0100

    added database support

--- chatApp.js
+++ chatApp.js
@@ -1,21 +1,34 @@
 const express = require('express');
 const http = require('http');
 const { Server } = require("socket.io");
+let credentialsLoader = require('./getCredentials');
+let mysqlSetup = require('./mysqlSetup');
 
 app = express()
+app.set('view engine', 'ejs');
+credentials = credentialsLoader.getCredentials();
+connection = mysqlSetup.getConnection();
+
+
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
+app.get('/', async (req, res) => {
+    // res.sendFile(__dirname + '/index.html');
+    let messages = await connection.asyncquery('SELECT * FROM chat');
+    console.log(messages)
+
+    res.render('main', {messages})
 });
 
 io.on('connection', (socket) => {
     console.log('a user connected');
 
-
     socket.on('chat message', (msg) => {
         console.log('message: ' + msg);
+        let query = connection.createQueryStringFromObject({table: 'chat', message: msg});
+        connection.asyncquery(query);
+        
         io.emit('chat message', msg);
     });
 
